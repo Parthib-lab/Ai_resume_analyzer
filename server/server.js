60cd08a93dcc092044ea860febbd0634cda1ca67
Parthib Mandal <parthibmandal69@gmail.com>
2026-04-13 23:27:43 +0530

    fix cors for frontend

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "npx plugins add vercel/vercel-plugin"
+    "ai-resume-analyzer-mpwaptw5m-parthib1.vercel.app"
   ],
   credentials: true
 }));
